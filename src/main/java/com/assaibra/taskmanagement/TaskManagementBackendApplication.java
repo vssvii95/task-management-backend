e8fd8d98a48e1aeabb1edcdd86b7579e7e5ced0b
vssvvii <ibragimktl95@gmail.com>
2025-12-30 12:48:30 +0500

    initial

---
+++ src/main/java/com/assaibra/taskmanagement/TaskManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package com.assaibra.taskmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagementBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskManagementBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/assaibra/taskmanagement/TaskManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assaibra.taskmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
